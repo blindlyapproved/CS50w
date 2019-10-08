dec094d7483c2783f7a79ea214daef82300dfd49
luukdej <l.dejonge@live.nl>
2019-10-08 18:14:46 +0200

    so many changes

--- lecture3/list.py
+++ lecture3/list.py
@@ -3,11 +3,11 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-engine = create_engine(os.getenv("DATABASE_URL"))
+engine = create_engine('postgresql://luuk:Jupiter@localhost:5432/lecture3')
 db = scoped_session(sessionmaker(bind=engine))
 
 def main():
-    flights = db.execute("SELECT origin, destiation, duration FROM flight").fetchall()
+    flights = db.execute("SELECT origin, destination, duration FROM flights").fetchall()
     for flights in flights:
         print(f"{flight.origin} to {flight.destination}), {flight.duration} minutes.")
 
